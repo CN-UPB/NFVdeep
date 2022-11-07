d1ee9e5bdb28bc9bad339863a180cb9d4a5c7cb7
Stefan Werner <36734964+stwerner97@users.noreply.github.com>
2022-11-07 14:42:54 +0100

    Fix issue in evaluation (#6)
    * fixing logging after reset()
    
    * fixing bandwidth cost coefficient
    
    * fixes evaluation & logging
    
    * bump version of sb3 to v1.5.0
    
    * changing build workflows
    
    * changing default scenario
    
    * black refactoring

--- graph_generator.py
+++ graph_generator.py
@@ -10,12 +10,12 @@ def generate_graph(file):
     G.add_node(0, cpu=3, memory=10.0, bandwidth=40.0)
     G.add_node(1, cpu=3, memory=25.0, bandwidth=40.0)
     G.add_node(2, cpu=10, memory=50.0, bandwidth=40.0)
-    #G.add_node(3, cpu=1, memory=1.0, bandwidth=10.0)
-    #G.add_node(4, cpu=3, memory=30.0, bandwidth=40.0)
+    # G.add_node(3, cpu=1, memory=1.0, bandwidth=10.0)
+    # G.add_node(4, cpu=3, memory=30.0, bandwidth=40.0)
     G.add_edge(0, 1, latency=50.0)
     G.add_edge(1, 2, latency=50.0)
-    #G.add_edge(2, 3, latency=50.0)
-    #G.add_edge(3, 4, latency=500.0)
+    # G.add_edge(2, 3, latency=50.0)
+    # G.add_edge(3, 4, latency=500.0)
 
     nx.write_gpickle(G, file)
 
@@ -37,19 +37,24 @@ def gml_reader(seed, cpu, memory, bandwidth, inputfile, outputfile):
 
     for num, node in enumerate(network.nodes()):
         mapping[node] = num
-        newnetwork.add_node(num, cpu=random.randint(
-            *cpu), memory=float(random.uniform(*memory)), bandwidth=float(random.uniform(*bandwidth)))
+        newnetwork.add_node(
+            num,
+            cpu=random.randint(*cpu),
+            memory=float(random.uniform(*memory)),
+            bandwidth=float(random.uniform(*bandwidth)),
+        )
 
     for e in network.edges():
         n1 = network.nodes(data=True)[e[0]]
         n2 = network.nodes(data=True)[e[1]]
-        n1_coord = np.array((n1['graphics'].get("x"), n1['graphics'].get("y")))
-        n2_coord = np.array((n2['graphics'].get("x"), n2['graphics'].get("y")))
+        n1_coord = np.array((n1["graphics"].get("x"), n1["graphics"].get("y")))
+        n2_coord = np.array((n2["graphics"].get("x"), n2["graphics"].get("y")))
 
-        distance = np.linalg.norm(n1_coord-n2_coord)
-        distance = distance/0.00062137  # miles->meter
-        delay = (distance / SPEED_OF_LIGHT * 1000) * \
-            PROPAGATION_FACTOR  # in milliseconds
+        distance = np.linalg.norm(n1_coord - n2_coord)
+        distance = distance / 0.00062137  # miles->meter
+        delay = (
+            distance / SPEED_OF_LIGHT * 1000
+        ) * PROPAGATION_FACTOR  # in milliseconds
 
         newnetwork.add_edge(mapping[e[0]], mapping[e[1]], latency=float(delay))
 
@@ -74,18 +79,22 @@ def graphml_reader(seed, cpu, memory, bandwidth, inputfile, outputfile):
 
     for num, node in enumerate(network.nodes()):
         mapping[node] = num
-        newnetwork.add_node(num, cpu=random.randint(
-            *cpu), memory=float(random.uniform(*memory)), bandwidth=float(random.uniform(*bandwidth)))
+        newnetwork.add_node(
+            num,
+            cpu=random.randint(*cpu),
+            memory=float(random.uniform(*memory)),
+            bandwidth=float(random.uniform(*bandwidth)),
+        )
 
     for e in network.edges():
         n1 = network.nodes(data=True)[e[0]]
         n2 = network.nodes(data=True)[e[1]]
         n1_lat, n1_long = n1.get("Latitude"), n1.get("Longitude")
         n2_lat, n2_long = n2.get("Latitude"), n2.get("Longitude")
-        distance = geodesic((n1_lat, n1_long),
-                            (n2_lat, n2_long)).meters  # in meters
-        delay = (distance / SPEED_OF_LIGHT * 1000) * \
-            PROPAGATION_FACTOR  # in milliseconds
+        distance = geodesic((n1_lat, n1_long), (n2_lat, n2_long)).meters  # in meters
+        delay = (
+            distance / SPEED_OF_LIGHT * 1000
+        ) * PROPAGATION_FACTOR  # in milliseconds
         newnetwork.add_edge(mapping[e[0]], mapping[e[1]], latency=float(delay))
 
     nx.write_gpickle(newnetwork, outputfile)
@@ -93,19 +102,20 @@ def graphml_reader(seed, cpu, memory, bandwidth, inputfile, outputfile):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--seed', type=int,  nargs='?',
-                        default=0)
-    parser.add_argument('--inputfile', type=str, nargs='?',
-                        const=1)
-    parser.add_argument('--outputfile', type=str, nargs='?',
-                        const=1, default=r'./data/network.gpickle')
+    parser.add_argument("--seed", type=int, nargs="?", default=0)
+    parser.add_argument("--inputfile", type=str, nargs="?", const=1)
+    parser.add_argument(
+        "--outputfile", type=str, nargs="?", const=1, default=r"./data/network.gpickle"
+    )
     args = parser.parse_args()
     cpu = (1, 500)
     memory = (1, 64)
     bandwidth = (1, 1000)
 
     if args.inputfile.endswith(".graphml"):
-        graphml_reader(args.seed, cpu, memory, bandwidth, args.inputfile, args.outputfile)
+        graphml_reader(
+            args.seed, cpu, memory, bandwidth, args.inputfile, args.outputfile
+        )
     if args.inputfile.endswith(".gml"):
         gml_reader(args.seed, cpu, memory, bandwidth, args.inputfile, args.outputfile)
     else:
--- nfvdeep/environment/env.py
+++ nfvdeep/environment/env.py
@@ -1,14 +1,13 @@
 import logging
+from copy import deepcopy
+
 import gym
-import json
 import numpy as np
 from gym import spaces
-from pathlib import Path
 from tabulate import tabulate
-from copy import deepcopy
-from environment.network import Network
-from environment.arrival import ArrivalProcess
-from environment.sfc import ServiceFunctionChain
+
+from nfvdeep.environment.network import Network
+from nfvdeep.environment.arrival import ArrivalProcess
 
 
 class Env(gym.Env):
@@ -24,15 +23,16 @@ class Env(gym.Env):
 
         # define action and statespace
         _, properties = Network.check_overlay(self.overlay_path)
-        num_nodes = properties['num_nodes']
-        num_node_resources = properties['num_node_resources']
+        num_nodes = properties["num_nodes"]
+        num_node_resources = properties["num_node_resources"]
 
         # action `num_nodes` refers to volutarily rejecting the VNF embedding
         self.action_space = spaces.Discrete(num_nodes + 1)
 
         obs_dim = num_nodes * num_node_resources + num_node_resources + 3
         self.observation_space = spaces.Box(
-            low=0.0, high=1.0, shape=(obs_dim,), dtype=np.float16)
+            low=0.0, high=1.0, shape=(obs_dim,), dtype=np.float16
+        )
 
         self.reward = 0
 
@@ -40,42 +40,50 @@ class Env(gym.Env):
         """Process the action of the agent.
 
         Args:
-          action(int): The action is the number of the server where the current VNF will be embedded to. 
+          action(int): The action is the number of the server where the current VNF will be embedded to.
           Action `|nodes|` refers to voluntarily rejecting an embedding of the VNF.
         """
 
-        assert(not self.done), 'Episode was already finished, but step() was called'
+        assert not self.done, "Episode was already finished, but step() was called"
 
-        info = {'accepted': False, 'rejected': False}
-        logging.debug('(SFC, VNF): {} -> Node: ({})'.format(
-            (self.sfc_idx, self.vnf_idx), action))
+        info = {"accepted": False, "rejected": False}
+        logging.debug(
+            "(SFC, VNF): {} -> Node: ({})".format((self.sfc_idx, self.vnf_idx), action)
+        )
 
         # get the, to be processed, VNF for the current SFC
         sfc = self.request_batch[self.sfc_idx]
         vnf = sfc.vnfs[self.vnf_idx]
 
-        '''
+        """
         Embed the VNF to the network. An embedding is invalid if either the VNF embedding or there do
         not remain sufficient resources to embed further VNFs of the current SFC / the SFC 
         will not satisfy its constraints (e.g. TTL constraint).
-        '''
+        """
         is_valid_sfc = self.vnf_backtrack.embed_vnf(sfc, vnf, action)
-        logging.debug('VNF embedding of {} to `vnf_backtrack` network on node: {} was {}.'.format(
-            self.vnf_idx, action, is_valid_sfc))
+        logging.debug(
+            "VNF embedding of {} to `vnf_backtrack` network on node: {} was {}.".format(
+                self.vnf_idx, action, is_valid_sfc
+            )
+        )
 
         if is_valid_sfc:
             # check whether a valid SFC embedding remains possible after embedding the VNF, i.e.
             # if sufficient resources remain and the SFC constraints (e.g. latency) can still be satisfied
             is_valid_sfc = self.vnf_backtrack.check_embeddable(
-                sfc, vnf_offset=self.vnf_idx + 1)
+                sfc, vnf_offset=self.vnf_idx + 1
+            )
 
-            logging.debug('SFC embedding (remains): {}.'.format(
-                'possible' if is_valid_sfc else 'impossible'))
+            logging.debug(
+                "SFC embedding (remains): {}.".format(
+                    "possible" if is_valid_sfc else "impossible"
+                )
+            )
 
-        '''
+        """
         Process the determined action. Backtrack to the latest network state if the action was invalid or progress 
         its state after a successful SFC embedding. 
-        '''
+        """
         # determine whether the, to be processed, VNF is the last VNF of the current SFC
         is_last_of_sfc = self.vnf_idx >= len(sfc.vnfs) - 1
 
@@ -85,12 +93,15 @@ class Env(gym.Env):
             self.sfc_idx, self.vnf_idx = (self.sfc_idx + 1, 0)
 
             # update info regarding successful embedding
-            info['accepted'] = True
-            info['placements'] = self.vnf_backtrack.sfc_embedding[sfc] 
-            info['sfc'] = sfc
+            info["accepted"] = True
+            info["placements"] = self.vnf_backtrack.sfc_embedding[sfc]
+            info["sfc"] = sfc
 
             logging.debug(
-                'Updating network state to `vnf_backtrack` after completing SFC: {}'.format(self.sfc_idx))
+                "Updating network state to `vnf_backtrack` after completing SFC: {}".format(
+                    self.sfc_idx
+                )
+            )
 
         # invoke the agent for the next VNF of the current SFC
         elif is_valid_sfc and not is_last_of_sfc:
@@ -101,31 +112,31 @@ class Env(gym.Env):
             self.vnf_backtrack = deepcopy(self.network)
 
             # update info regarding unsuccessful embedding
-            info['rejected'] = True
-            info['placements'] = None 
-            info['sfc'] = sfc
+            info["rejected"] = True
+            info["placements"] = None
+            info["sfc"] = sfc
 
             self.sfc_idx, self.vnf_idx = (self.sfc_idx + 1, 0)
 
-            logging.debug(
-                'Backtracking `vnf_backtrack` to the latest network state')
+            logging.debug("Backtracking `vnf_backtrack` to the latest network state")
 
-        logging.debug('SFC, VNF indices set to: {}, {}'.format(
-            self.sfc_idx, self.vnf_idx))
+        logging.debug(
+            "SFC, VNF indices set to: {}, {}".format(self.sfc_idx, self.vnf_idx)
+        )
 
-        '''
+        """
         Progress time such that the agent will only be invoked to embed SFC that are embeddable, i.e. for whom
         a valid sequence of VNF embeddings exist.
-        '''
+        """
         # progress to next SFC within the request batch that can be embedded succsessfully
         # , i.e. progress within the intra timeslot
         batch_completed = self.progress_intra_timeslot()
 
         # determine the agent's reward before progressing time
         self.reward = self.compute_reward(
-            sfc, is_last_of_sfc, is_valid_sfc, batch_completed)
-        logging.debug(
-            'Environment will attribute reward: {}'.format(self.reward))
+            sfc, is_last_of_sfc, is_valid_sfc, batch_completed
+        )
+        logging.debug("Environment will attribute reward: {}".format(self.reward))
 
         # progress inter timeslots until a `request_batch` with embeddable SFCs arrives
         if batch_completed:
@@ -141,12 +152,12 @@ class Env(gym.Env):
         # log the occupied resources and operation costs, log the number of operating nodes
         resource_utilization = self.vnf_backtrack.calculate_resource_utilization()
         resource_costs = self.vnf_backtrack.calculate_resource_costs()
-        info.update({res + '_utilization': val for res,
-                     val in resource_utilization.items()})
-        info.update({res + '_cost': val for res,
-                     val in resource_costs.items()})
+        info.update(
+            {res + "_utilization": val for res, val in resource_utilization.items()}
+        )
+        info.update({res + "_cost": val for res, val in resource_costs.items()})
         num_operating = len(self.vnf_backtrack.get_operating_servers())
-        info.update({'operating_servers': num_operating})
+        info.update({"operating_servers": num_operating})
 
         return self.compute_state(done=self.done), self.reward, self.done, info
 
@@ -173,8 +184,8 @@ class Env(gym.Env):
         return self.compute_state()
 
     def progress_intra_timeslot(self):
-        """ Progress (SFC, VNF) indices to the next combination in the request batch that requires an 
-        invokation of the agent. 
+        """Progress (SFC, VNF) indices to the next combination in the request batch that requires an
+        invokation of the agent.
         """
 
         # progress the SFC indices to a SFC for whom an interaction with the agent is required,
@@ -195,8 +206,7 @@ class Env(gym.Env):
         return batch_completed
 
     def progress_inter_timeslots(self):
-        """ Progress `request_batch` and `network` over inter timeslots, i.e. until novel requests arrive.
-        """
+        """Progress `request_batch` and `network` over inter timeslots, i.e. until novel requests arrive."""
         # empty the `request_batch` since we assume that all requests have been processed
         self.request_batch = []
 
@@ -205,8 +215,11 @@ class Env(gym.Env):
                 self.network.update()
                 self.request_batch = next(self.arrival_process)
 
-                logging.debug('Progressing time, next SFC request batch: {}'.format(
-                    self.request_batch))
+                logging.debug(
+                    "Progressing time, next SFC request batch: {}".format(
+                        self.request_batch
+                    )
+                )
 
             # initialize backtracking for the invoked intra timeslot
             self.vnf_backtrack = deepcopy(self.network)
@@ -216,45 +229,90 @@ class Env(gym.Env):
             # the `arrival_process` is done generating further requests, finish the episode
             return True
 
-    def render(self, mode='human'):
+    def render(self, mode="human"):
         req_batch = str(self.request_batch)
-        resources = [[num, *[str(avail_res) + ' out of ' + str(max_res) for avail_res, max_res in zip(res[0].values(), res[1].values())]]
-                     for num, res in enumerate(zip(self.vnf_backtrack.calculate_resources(), self.vnf_backtrack.calculate_resources(False)))]
+        resources = [
+            [
+                num,
+                *[
+                    str(avail_res) + " out of " + str(max_res)
+                    for avail_res, max_res in zip(res[0].values(), res[1].values())
+                ],
+            ]
+            for num, res in enumerate(
+                zip(
+                    self.vnf_backtrack.calculate_resources(),
+                    self.vnf_backtrack.calculate_resources(False),
+                )
+            )
+        ]
 
-        sfcs = [[res.arrival_time, res.ttl, res.bandwidth_demand, res.max_response_latency, '\n'.join([str(vnf) for vnf in res.vnfs]), '\n'.join([str(node) for node in nodes])]
-                for res, nodes in self.vnf_backtrack.sfc_embedding.items()]
+        sfcs = [
+            [
+                res.arrival_time,
+                res.ttl,
+                res.bandwidth_demand,
+                res.max_response_latency,
+                "\n".join([str(vnf) for vnf in res.vnfs]),
+                "\n".join([str(node) for node in nodes]),
+            ]
+            for res, nodes in self.vnf_backtrack.sfc_embedding.items()
+        ]
 
-        rep = str(tabulate(resources, headers=[
-                  'Node', 'Cpu', 'Memory', 'Bandwidth'], tablefmt="presto"))
-        rep += '\n \n \n'
-        rep += 'Currently active Service Function Chains in the network:\n \n'
-        rep += str(tabulate(sfcs, headers=['Arrival time', 'TTL', 'Bandwidth',
-                                           'Max latency', 'VNFs (CPU, Memory)', ' Embedding Nodes'], tablefmt="grid"))
-        rep += '\n \n'
-        rep += 'Last reward = '+str(self.reward)+'\n'
-        rep += 'Current request batch for (network) timestep ' + str(
-            self.network.timestep)+': \n' + str(req_batch)
-        rep += '\n \n \n'
-        rep += '======================================================================================================================'
-        rep += '\n'
+        rep = str(
+            tabulate(
+                resources,
+                headers=["Node", "Cpu", "Memory", "Bandwidth"],
+                tablefmt="presto",
+            )
+        )
+        rep += "\n \n \n"
+        rep += "Currently active Service Function Chains in the network:\n \n"
+        rep += str(
+            tabulate(
+                sfcs,
+                headers=[
+                    "Arrival time",
+                    "TTL",
+                    "Bandwidth",
+                    "Max latency",
+                    "VNFs (CPU, Memory)",
+                    " Embedding Nodes",
+                ],
+                tablefmt="grid",
+            )
+        )
+        rep += "\n \n"
+        rep += "Last reward = " + str(self.reward) + "\n"
+        rep += (
+            "Current request batch for (network) timestep "
+            + str(self.network.timestep)
+            + ": \n"
+            + str(req_batch)
+        )
+        rep += "\n \n \n"
+        rep += "======================================================================================================================"
+        rep += "\n"
         return rep
 
     def compute_state(self, done=False):
         """Compute the environment's state representation."""
 
-        if (done == True):
-            return np.asarray([])
+        if done:
+            return np.zeros(self.observation_space.shape)
 
         # compute remaining resources of backtrack network, i.e. (cpu, memory, bandwidth) for each node
-        network_resources = self.vnf_backtrack.calculate_resources(
-            remaining=True)
-        network_resources = np.asarray([list(node_res.values())
-                                        for node_res in network_resources], dtype=np.float16)
+        network_resources = self.vnf_backtrack.calculate_resources(remaining=True)
+        network_resources = np.asarray(
+            [list(node_res.values()) for node_res in network_resources],
+            dtype=np.float16,
+        )
 
         # normalize network resources by their max capacity (over all nodes)
         max_resources = self.vnf_backtrack.calculate_resources(remaining=False)
-        max_resources = np.asarray([list(node_res.values())
-                                    for node_res in max_resources], dtype=np.float16)
+        max_resources = np.asarray(
+            [list(node_res.values()) for node_res in max_resources], dtype=np.float16
+        )
         max_resources = np.max(max_resources, axis=0)
         network_resources = network_resources / max_resources
         # reshaping to 1d vector (flatten)
@@ -268,13 +326,22 @@ class Env(gym.Env):
         norm_vnf_resources = list(norm_vnf_resources / max_resources)
 
         # TODO: parameterize normalization of constants such as TTL?
-        norm_residual_latency = (sfc.max_response_latency -
-                                 self.vnf_backtrack.calculate_current_latency(sfc)) / 1000
+        norm_residual_latency = (
+            sfc.max_response_latency - self.vnf_backtrack.calculate_current_latency(sfc)
+        ) / 1000
         norm_undeployed = (len(sfc.vnfs) - (self.vnf_idx + 1)) / 7
         norm_ttl = sfc.ttl / 1000
 
-        observation = np.concatenate((network_resources,
-                                      norm_vnf_resources, norm_residual_latency, norm_undeployed, norm_ttl), axis=None)
+        observation = np.concatenate(
+            (
+                network_resources,
+                norm_vnf_resources,
+                norm_residual_latency,
+                norm_undeployed,
+                norm_ttl,
+            ),
+            axis=None,
+        )
 
         return observation
 
@@ -289,7 +356,7 @@ class Env(gym.Env):
         if not (last_in_sfc and sfc_valid):
             reward = 0
         else:
-            reward = (sfc.ttl*sfc.bandwidth_demand) / 10
+            reward = (sfc.ttl * sfc.bandwidth_demand) / 10
 
         if batch_completed:
             # compute total operation costs of the network first from individual costs of each resource
